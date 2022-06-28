cf072aa301359c25064ab202542d71b68d245625
Bagas12345678 <106974492+Bagas12345678@users.noreply.github.com>
2022-06-28 07:24:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['62881010244718'],
+  ['62881010244718'],
+  ['62881010244718', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
